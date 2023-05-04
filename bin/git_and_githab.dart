7ecfc8c9ccfb1bfd0ae0ede089a13d1f994a93aa
Oskaroff <yurakutenko@gmail.com>
2023-05-04 21:29:19 +0300

    first commit

---
+++ bin/git_and_githab.dart
@@ -0,0 +1,5 @@
+import 'package:git_and_githab/git_and_githab.dart' as git_and_githab;
+
+void main(List<String> arguments) {
+  print('Hello world: ${git_and_githab.calculate()}!');
+}
---
+++ lib/git_and_githab.dart
@@ -0,0 +1,37 @@
+// https://github.com/Oskaroff/shporgalka_gi.git
+// Плагин  - Git History Diff
+
+// ----------------      Команды:  ----------------------
+// -------------------------------------------------------
+// git --version
+// dir - показывает что есть в папке
+
+
+// Начальная настройка
+// git init - создает репазиторий git (папку)
+// git config --global user.name "Oskaroff"
+// git config --global user.email yurakutenko@gmail.com
+// git remote add origin https://github.com/Oskaroff/shporgalka_gi.git
+// git push -u origin main (или master ветка) - заливаем все на GitHab
+
+// git add . - добавить все в stage
+// git add "main.dart" - добавляем конкретный файл в stage
+// git commit -m "first commit"
+// git branch - показывает на какой ветке мы находимся
+// git branch -M main - переименовывает master в main
+// git branch -D vetka - удаляем ветку
+// git branch vetka  - создаем новую ветку
+// git checkout vetka - переходим на ветку
+// git merge vetka - слияние веток
+
+// git status - проверяем статус
+// git push - заливаем на сервер 
+// git log - используется для просмотра истории коммитов,
+
+// git pull - подгружает все изменения которые были в репозитории
+// git checkout -- main.dart или с точкой - отмена последних действий. До stage
+// git reset main.dart - отмена последних действий. После Stage
+
+// Если удалили проект на компе , можно его восстановить с Git HUB
+// git clone https://github.com/Oskaroff/shporgalka_gi.git
+//
