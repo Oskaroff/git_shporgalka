3edea390dcd6a75e62adf1735570b1264501912f
Oskaroff <yurakutenko@gmail.com>
2023-05-04 21:34:54 +0300

    first commit

--- bin/git_and_githab.dart
+++
@@ -1,5 +0,0 @@
-import 'package:git_and_githab/git_and_githab.dart' as git_and_githab;
-
-void main(List<String> arguments) {
-  print('Hello world: ${git_and_githab.calculate()}!');
-}
--- test/git_and_githab_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:git_and_githab/git_and_githab.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
